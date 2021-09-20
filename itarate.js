53626cea92184e11224aa3802d21198d0c84681c
tufaelahmedbd <tofayelahmed2000@gmail.com>
2021-09-21 03:07:47 +0600

    itarate

---
+++ itarate.js
@@ -0,0 +1,15 @@
+// First object itarate
+// object itarate in javascript 
+// objecat declearation 
+let object={
+    x:5,
+    y:6,
+    z:9
+}
+// console.log('x' in object )
+// for loop run on this programme
+for(let i in object){
+    console.log(i)
+    console.log(i + ":"+object[i])
+} 
+// Second object itarate
